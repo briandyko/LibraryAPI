a7795a145a2659861fdc11644f315e3d9be34f51
Brian Dyko <32283478+briandyko@users.noreply.github.com>
2020-08-10 20:46:12 +0000

    done with Monday

--- LibraryApiIntegrationTests/WebTestFixture.cs
+++ LibraryApiIntegrationTests/WebTestFixture.cs
@@ -1,9 +1,11 @@
 ﻿using LibraryApi;
+using LibraryApi.Domain;
 using LibraryApi.Services;
 using LibraryApiIntegrationTests.Fakes;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -29,6 +31,30 @@ namespace LibraryApiIntegrationTests
                     services.Remove(systemTimeDescriptor);
                     services.AddTransient<ISystemTime, TestingSystemTime>();
                 }
+
+                var dbContextDescriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<LibraryDataContext>)
+                );
+
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                    services.AddDbContext<LibraryDataContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("JustAUniqueName");
+                    });
+
+                    var sp = services.BuildServiceProvider();
+
+                    using var scope = sp.CreateScope();
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<LibraryDataContext>();
+                    db.Database.EnsureCreated(); // basically "update-database"
+
+                    // TODO: Add Some Data
+                    DataUtils.ReinitializeDb(db);
+                }
+
 
             });
         }
